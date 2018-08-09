5a88ddf20d5bc7f0dd4c3f03d6c74205bcdcea61
Adaming <Adaming@192.168.1.148>
2018-08-09 09:37:15 +0200

    claire: commit classe Produit

--- Projet_ECommerce/src/fr/adaming/model/Produit.java
+++ Projet_ECommerce/src/fr/adaming/model/Produit.java
@@ -2,4 +2,85 @@ package fr.adaming.model;
 
 public class Produit {
 
+	//Attributs
+	private Long id;
+	private String designation;
+	private String description;
+	private Double prix;
+	private int quantite;
+	private boolean selectionne;
+	private String photo;
+	
+	//Constructeurs
+	public Produit() {
+		super();
+	}
+	public Produit(String designation, String description, Double prix, int quantite, boolean selectionne,
+			String photo) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	public Produit(Long id, String designation, String description, Double prix, int quantite, boolean selectionne,
+			String photo) {
+		super();
+		this.id = id;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	
+	//Getters et setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrix() {
+		return prix;
+	}
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
+	
 }
