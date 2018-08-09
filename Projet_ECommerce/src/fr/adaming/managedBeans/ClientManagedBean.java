bf26d56c1475ecc5f451a904c072d1c6941f4abc
Adaming <Adaming@192.168.1.148>
2018-08-09 15:48:14 +0200

    claire: ajouter client + aff liste client

--- Projet_ECommerce/src/fr/adaming/managedBeans/ClientManagedBean.java
+++ Projet_ECommerce/src/fr/adaming/managedBeans/ClientManagedBean.java
@@ -1,29 +1,35 @@
 package fr.adaming.managedBeans;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.component.message.Message;
 
 import fr.adaming.Service.IClientService;
 import fr.adaming.model.Client;
-@ManagedBean(name="clMB")
+
+@ManagedBean(name = "clMB")
 @RequestScoped
-public class ClientManagedBean implements Serializable{
+public class ClientManagedBean implements Serializable {
 	/**
 	 * Transformation de l'association UML en JAVA et injection dependance
 	 */
 	@EJB
 	private IClientService clService;
-	
-	/** 
+
+	/**
 	 * Declaration des attributs
 	 */
 	private Client client;
 
 	/**
-	 * Declaration du constructeur vide 
+	 * Declaration du constructeur vide
 	 */
 	public ClientManagedBean() {
 		super();
@@ -41,16 +47,28 @@ public class ClientManagedBean implements Serializable{
 	}
 
 	/**
-	 * @param client the client to set
+	 * @param client
+	 *            the client to set
 	 */
 	public void setClient(Client client) {
 		this.client = client;
 	}
-	
-	/** Methodes du client*/
-	public String enregistrerClient(){
-		
+
+	/** Methodes du client */
+	public String enregistrerClient() {
+		Client clEnr = clService.enregistrerClient(client);
+
+		if (clEnr.getIdClient() != 0) {
+			/** Recuperer la liste */
+			List<Client> listeCl = clService.getAllClient();
+
+			return "affCl";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("L'enregistrement du client a echoué"));
+			
+			return "enregistrement";
+		}
+
 	}
-	
 
 }
