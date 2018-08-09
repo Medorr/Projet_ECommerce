a4ad482b01983f4c7cdfe2208e0442bf0541c903
INTI0488 <INTI0488@192.168.1.93>
2018-08-09 12:39:38 +0200

    Demba: Doa categorie

--- Projet_ECommerce/src/fr/adaming/dao/CategorieDaoImpl.java
+++ Projet_ECommerce/src/fr/adaming/dao/CategorieDaoImpl.java
@@ -1,5 +1,42 @@
 package fr.adaming.dao;
 
-public class CategorieDaoImpl {
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.management.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
+import fr.adaming.model.Admin;
+import fr.adaming.model.Categorie;
+@Stateless //** Pour la methode DAO, on utilie stateless pour qu'il garde son etat*/
+public class CategorieDaoImpl implements ICategorieDao{
+	
+	/**On utilise EJB avec l'annotation JPA*/
+	@PersistenceContext(unitName="PU")
+	private EntityManager em;
+	
+//	/** Recuperer la liste des categorie avec SQL*/
+//	/**Creation de EMF*/
+//	EntityManagerFactory emf=Persistence.createEntityManagerFactory("PU");
+//	
+//	/**Creation de EM*/
+//	EntityManager em=emf.createEntityManager();
+
+	@Override
+	public List<Categorie> getAllCategorie() {		
+		/**Creation de la req SQL*/
+		String reqListSQL="SELECT*FROM categories";
+		
+		/**Creation de la liste de Catégorie*/
+		Query queryListSQL=(Query) em.createNativeQuery(reqListSQL, Categorie.class);
+		
+		//** Recuperer la nouvelle liste des catégorie*/
+		List<Categorie> listeSQL=queryListSQL.getResultList();		
+		
+		return query.getResultatList();
+	}
 
 }
--- Projet_ECommerce/src/fr/adaming/dao/ICategorieDao.java
+++ Projet_ECommerce/src/fr/adaming/dao/ICategorieDao.java
@@ -1,5 +1,16 @@
 package fr.adaming.dao;
 
+import java.util.List;
+
+import javax.ejb.Local;
+
+import fr.adaming.model.Admin;
+import fr.adaming.model.Categorie;
+
+@Local //**On utilise local pour que l'appli soit accecible dans le serveur a chaque utilisation*/
 public interface ICategorieDao {
+	
+	public List<Categorie> getAllCategorie();
+	
 
 }
